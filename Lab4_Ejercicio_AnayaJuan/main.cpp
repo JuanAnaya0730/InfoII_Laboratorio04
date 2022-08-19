84cb72ebc40cb33cb0fdce84ea7f35b6e3509990
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-19 07:04:02 -0500

    se creo la interaccion de cargar una red

--- Lab4_Ejercicio_AnayaJuan/main.cpp
+++ Lab4_Ejercicio_AnayaJuan/main.cpp
@@ -5,5 +5,19 @@ using namespace std;
 
 int main()
 {
+    network _network_;
+    int option = menu(0);
+    string aux;
+
+    if(option == 1){
+        cout << "Ingrese el nombre del archivo donde se encuentra la red\n -> ";
+        getline(cin, aux);
+
+        _network_.load_network(aux);
+
+    }else{
+        cout << "red aleatoria aun no se hace XD" << endl;
+    }
+
     return 0;
 }
