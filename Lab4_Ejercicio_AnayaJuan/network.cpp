83288d119102d71c6a1ebc300ce39149a3bbcc4d
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-19 04:29:56 -0500

    se crearon los prototipos de las funciones que se en cargaran de leer y guardar una red en un archivo, tambien se añadio un nuevo atributo, este es el nombre del archivo al que esta asociado la red

--- Lab4_Ejercicio_AnayaJuan/network.cpp
+++ Lab4_Ejercicio_AnayaJuan/network.cpp
@@ -4,3 +4,24 @@ network::network()
 {
 
 }
+
+void network::load_network()
+{
+
+}
+
+void network::save_network()
+{
+
+}
+
+string network::read()
+{
+
+}
+
+void network::write()
+{
+
+}
+
--- Lab4_Ejercicio_AnayaJuan/network.h
+++ Lab4_Ejercicio_AnayaJuan/network.h
@@ -9,10 +9,17 @@ using namespace std;
 class network
 {
 private:
+    string file;
     vector<router> topology;
+
+    string read();
+    void write();
 
 public:
     network();
+
+    void load_network();
+    void save_network();
 };
 
 #endif // NETWORK_H
