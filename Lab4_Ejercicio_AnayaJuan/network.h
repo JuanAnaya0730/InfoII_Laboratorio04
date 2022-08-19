b3837c5209524ad5b934a531dcf43d8c91a0913d
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-19 00:48:51 -0500

    se añadio la libreria router y vector, tambien se creo un atributo que sera la topologia de la red

--- Lab4_Ejercicio_AnayaJuan/network.h
+++ Lab4_Ejercicio_AnayaJuan/network.h
@@ -1,9 +1,16 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include <vector>
+#include "router.h"
+
+using namespace std;
 
 class network
 {
+private:
+    vector<router> topology;
+
 public:
     network();
 };
