5c829df59a45583259e3649dbdebe345238abac9
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-19 07:21:12 -0500

    se creo el cuerpo de interacciones restantes

--- Lab4_Ejercicio_AnayaJuan/main.cpp
+++ Lab4_Ejercicio_AnayaJuan/main.cpp
@@ -11,12 +11,30 @@ int main()
 
     if(option == 1){
         cout << "Ingrese el nombre del archivo donde se encuentra la red\n -> ";
-        getline(cin, aux);
+        getline(cin, aux); system("cls");
 
         _network_.load_network(aux);
 
     }else{
         cout << "red aleatoria aun no se hace XD" << endl;
+    }
+
+    while(true){
+        option = menu(1);
+
+        if(option == 1){
+            cout << "agregar enrutador" << endl;
+
+        }else if(option == 2){
+            cout << "eliminar enrutador" << endl;
+
+        }else if(option == 3){
+            cout << "Costo de envio" << endl;
+
+        }else if(option == 4){
+            cout << "Camino de envio" << endl;
+
+        }else{ break; };
     }
 
     return 0;
